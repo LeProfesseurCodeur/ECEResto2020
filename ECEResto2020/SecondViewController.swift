7bff9d2fd02307fc4c54c1dd172776e798622861
Dylan LUCHMUN <dylan.luchmun@epitech.eu>
2020-01-08 09:42:26 +0100

    initialisation projet avec design

---
+++ ECEResto2020/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ECEResto2020
+//
+//  Created by Dylan Luchmun on 08/01/2020.
+//  Copyright © 2020 Dylan Luchmun. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
